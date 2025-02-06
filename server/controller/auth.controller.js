9dc80d82b74741abc7f7ae51e4fc6fd24c575987
Prudvi <prudvirajvarma2004@gmail.com>
2025-02-06 15:40:13 +0530

    added middlewares

--- server/controller/auth.controller.js
+++ server/controller/auth.controller.js
@@ -1,10 +1,11 @@
 import express from "express"
 import { login, logout, signin } from "../routes/auth.route.js"
+import { protectRoute } from "../lib/middleware.js"
 
 const router = express.Router()
 
 router.post("/signin",signin)
-router.post("/login",login)
+router.post("/login",protectRoute,login)
 router.post("/logout",logout)
 
 export default router
---
+++ server/lib/middleware.js
@@ -0,0 +1,33 @@
+import jwt from "jsonwebtoken"
+import User from "../models/user.model.js"
+
+export const protectRoute = async (req, res, next) => {
+
+    try {
+        const token = req.cookies["jwt-paytm"]
+        if (!token) {
+            return res.status(401).json({
+                msg: "No token"
+            })
+        }
+        const decoded = await jwt.verify(token, process.env.JWT_SECRET)
+
+        if(!decoded){
+            return res.status(401).json({
+                msg: "Invalid Token"
+            })
+        }
+
+        const user = await User.findById(decoded.id).select("-password")
+
+        if(!user){
+            return res.json("User not found")
+        }
+
+        req.user = user
+
+        next()
+    } catch (error) {
+        console.log("Error in protect route");
+    }
+}
